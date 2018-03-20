928ee6621c876f7d8e7eb37e30be7c0e59cb9ada
Lauren Corlies <dalek@Laurens-MacBook-Pro.local>
2018-03-20 11:11:46 -0400

    functions for helping with shiftc on lou

---
+++ lou_file_support.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def prepare_shiftc_file(start,stop,ref=True):
+    DDs = np.arange(start,stop,1)
+    fout = open('untar.exe','w')
+    for DD in DDs:
+        if DD < 1000:
+            DDstr = 'DD0'+str(DD)
+        else:
+            DDstr = 'DD'+str(DD)
+        if ref == True:
+            dirout = '/lou/s2m/lcorlies/nref11n/nref10_refine200kpc_z2to1/'
+        else:
+            dirout = '/lou/s2m/lcorlies/nref11n/natural'
+        lineout = 'shiftc --extract-tar '+DDstr+'.tar '+dirout+' \n'
+        fout.write(lineout)
+    fout.close()
+    return
+
+
+#shiftc --extract-tar DD0354.tar
