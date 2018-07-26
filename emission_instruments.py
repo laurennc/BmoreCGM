fixed units issue to get code to run!

emission_instruments.py
@@ -86,7 +86,7 @@ class EmissionMap:
 
       ## Now set up projection region
       ## Area should be size of FOV and not the refine region scaled down
-      FOV = self.ds.arr(FOV,'kpc').to('code_length')
+      FOV = self.ds.arr(FOV,'kpc').to('code_length').value
       LE = np.copy(self.rb_center)
       LE[area_axes] -= FOV/2.
       LE[proj_axis] -= self.rb_width/2.
@@ -96,8 +96,8 @@ class EmissionMap:
 
       box = self.ds.r[LE[0]:RE[0],LE[1]:RE[1],LE[2]:RE[2]]
       obj = self.ds.proj(('gas',field),proj_axis,data_source=box)
-      frb = obj.to_frb((FOV[0].value,'code_length'),(int(num_cells[0]),int(num_cells[1])),
-                        height=(FOV[1].value,'code_length'),center=self.rb_center)
+      frb = obj.to_frb((FOV[0],'code_length'),(int(num_cells[0]),int(num_cells[1])),
+                        height=(FOV[1],'code_length'),center=self.rb_center)
       cPickle.dump(frb[('gas',field)],open(pickle_out,'wb'),protocol=-1)
       return frb
 
@@ -108,10 +108,7 @@ class EmissionMap:
     if self.check_line_in_bandpass(line):
         ## NEED TO SET PLOT SIZE SO THAT IT'S CONSISTENT
         ## BETWEEN ALL OF THE INSTRUMENTS
-        ## FIX THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        #frb = cPickle.load(open(frb,'rb'))
-
-            ## Convert to the units most observers use
+        ## Convert to the units most observers use
         lenergy = line_energies[line]
         frb = frb.to('s**-1*arcsec**-2*cm**-2')*lenergy
         frb = frb/(1.+self.redshift)**4
@@ -126,14 +123,14 @@ class EmissionMap:
         ## it's going to generate two different color bars
         ## it may be better to make a seaborn colorbar for eac
         ## but let's start somewhere
-        sb_lim = self.instrument[self.mode]['SBlim']
-        sb_lim = 1./(u.cm**2*u.s*u.steradian)
-        obs = np.ma.masked_where((frb > sb_lim),frb)
-        obs_frb = frb*obs.mask
+        #sb_lim = self.instrument[self.mode]['SBlim']
+        #sb_lim = 1./(u.cm**2*u.s*u.steradian)
+        #obs = np.ma.masked_where((frb > sb_lim),frb)
+        #obs_frb = frb*obs.mask
 
-        plt.imshow(np.log10(frb),extent=(bd1,bd2,bd3,bd4),vmin=-25,vmax=-17,
-                   origin='lower',interpolation=None,cmap='bone')
-        ax.imshow(np.log10(obs_frb),vmin=np.log10(sb_lim),vmax=-14,cmap=self.instrument[self.mode]['cmap'])
+        plt.imshow(np.log10(frb),extent=(bd1,bd2,bd3,bd4),vmin=-25,vmax=-19,
+                   origin='lower',interpolation=None)#,cmap='bone')
+        #ax.imshow(np.log10(obs_frb),vmin=np.log10(sb_lim),vmax=-14,cmap=self.instrument[self.mode]['cmap'])
         plt.colorbar()
         plt.savefig(fileout)
         plt.close()
@@ -142,6 +139,10 @@ class EmissionMap:
         plt.savefig(fileout)
         plt.close()
     return
+
+
+  def plot_frb_talks(self,line,frb,fileout):
+      return
 
   def check_line_in_bandpass(self,line):
         if isinstance(line,str):