6aad2486d8e8fed910bdf36ff689a214d8a2cfe1
Lauren Corlies <dalek@Laurens-MacBook-Pro.local>
2018-08-23 16:19:05 -0400

    working on making the right sized plots for the paper

--- run_emission_instruments.py
+++ run_emission_instruments.py
@@ -3,8 +3,8 @@ import numpy as np
 import cPickle
 from emission_instruments import *
 
-#base = "/Users/dalek/data/Molly/nref11n_nref10f_refine200kpc_z4to2"
-base = "/Users/dalek/data/Molly/natural/nref11"
+base = "/Users/dalek/data/Molly/nref11n_nref10f_refine200kpc_z4to2"
+#base = "/Users/dalek/data/Molly/natural/nref11"
 fn = base+"/RD0016/RD0016"
 track_name = base+"/halo_track"
 ds = yt.load(fn)
@@ -19,6 +19,6 @@ for line in lines:
     inst = instr_dict[instrument]
     for mode in inst.keys():
       emap = EmissionMap(ds,track,instrument,mode)
-      frb = emap.make_frb(line,'x','instruments/emis_RD0016NAT_'+instrument+'_'+mode+'_'+line+'.cpkl')
+      frb = emap.make_frb(line,'z','instruments/emis_z_RD0016REF_'+instrument+'_'+mode+'_'+line+'.cpkl')
       frb = frb['gas','Emission_'+line]
-      emap.plot_frb(line,frb,'instruments/emis_RD0016NAT_'+instrument+'_'+mode+'_'+line+'.pdf')
+      emap.plot_frb_talks(line,frb,instrument,'instruments/green_obs/emis_z_RD0016REF_'+instrument+'_'+mode+'_'+line+'.pdf')
